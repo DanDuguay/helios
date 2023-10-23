e38073efe4f38ad61b741989a5f712a2336c77e6
amir <amd.gharibipour@outlook.com>
2023-10-23 18:15:03 -0400

    updated add style

--- src/pages/Add.vue
+++ src/pages/Add.vue
@@ -3,51 +3,100 @@
 
 <template>
     <div :class="$style.container">
-        <div :class="$style.form">
-            <p :class="$style.title">You can drag and drop files here to add them</p>
-            <button :class="$style.add">Add</button>
+
+        <p :class="$style.title">Upload file</p>
+
+        <div :class="$style.inputSection">
+            <input type="file" :class="$style.input"/>
+            <button :class="$style.uploadBtn">
+                <font-awesome-icon :icon="['fas', 'plus']" />
+                Upload
+            </button>
         </div>
+
+        <div :class="$style.fileSection">    
+            <p :class="$style.fileItem">
+                <font-awesome-icon :class="$style.icon" :icon="['fas', 'file']" />
+                File1.pdf
+                <font-awesome-icon :class="$style.trash" :icon="['fas', 'trash']" />
+            </p>
+            <p :class="$style.fileItem">
+                <font-awesome-icon :class="$style.icon" :icon="['fas', 'file']" />
+                File2.pdf
+                <font-awesome-icon :class="$style.trash" :icon="['fas', 'trash']" />
+            </p>
+        </div>
+
     </div>
 </template>
 
 <style module>
 .container{
     display:flex;
-    width:100%;
-    height: 100vh;
-}
-.input{
-    visibility: hidden;
-}
-.form{
-    border-style: dashed;
-    background-color: rgba(0,0,0,0.2);
-    cursor: pointer;
-    border-width: 5px;
-    border-color: #f9826C;
-    display: flex;
+    width:70%;
     flex-direction: column;
     margin: auto;
-    max-width: 800px;
-    max-height: 800px;
-    width: 60vw;
-    height: 60vw;
-    border-radius: 10px;
+    margin-top: 15vh;
+}
+.inputSection{
+    background-color: rgba(5,5,5,0.1);
+    display: flex;
+    border-radius: 5px;
+    height: 120px;
+    border-style: dashed;
+    border-width: 3px;
+    border-color: rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+}
+.input{
+    display: none;
+}
+.fileSection{
+    display: flex;
+    flex-direction: column;
+    margin-top: 10px;
 }
 .title{
-    font-weight: 700;
-    margin: auto;
-    margin-bottom: 15px;
-    font-size: large;
+    font-weight: 600;
 }
-.add{
-    width: 300px;
-    height: 50px;
-    background-color: #f9826C;
-    cursor: pointer;
-    border: none;
-    border-radius: 10px;
+.uploadBtn{
+    width: 45%;
+    background-color: rgb(249,130,108);
+    border:none;
+    height: 35px;
     margin: auto;
-    margin-top: 15px;
+    font-size: 18px;
+    cursor: pointer;
+    font-weight: 600;
+    border-radius: 5px;
+}
+
+.fileItem{
+    background-color: rgba(249,130,108,0.2);
+    color: #000;
+    font-weight: 500;
+    border-radius: 5px;
+    margin-top: 10px;
+    margin-bottom: 5px;
+    padding: 10px;
+    align-items: center;
+    display: flex;
+    transition: 0.5s;
+}
+.fileItem:hover{
+    margin-top: 7px;
+    margin-bottom: 8px;
+}
+
+.icon{
+    color:rgb(249,130,108);
+    margin-left: 5px;
+    margin-right: 20px;
+}
+.trash{
+    color:rgb(249,130,108);
+    margin-right: 5px;
+    cursor: pointer;
+    margin-left: auto;
 }
 </style>
